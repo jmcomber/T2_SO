06351b82e80c96b26a3b6408931391ca47db9ee0
jmcomber <jmcomber1@gmail.com>
2018-04-17 20:38:02 -0300

    inicio parseo

--- doer/main.c
+++ doer/main.c
@@ -1,5 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <signal.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+void imprimir_estadisticas() {
+
+}
 
 
 int main(int argc, char * argv []) {
-	
+	signal(SIGINT, imprimir_estadisticas);
+	char *path = argv[1];
+	int n = atoi(argv[2]);
 }
