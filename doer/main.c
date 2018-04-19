8a6c446ee4c63bebb17480a74130908ab996d4a5
jmcomber <jmcomber1@gmail.com>
2018-04-19 15:21:20 -0300

    pensamientos + tasks

--- doer/main.c
+++ doer/main.c
@@ -24,10 +24,28 @@ void terminar() {
 
 int main(int argc, char * argv []) {
 	signal(SIGINT, terminar);
+	// char cmd[10] = "echo";
+	// char *args[] = {cmd, "sss", NULL};
+	// execvp(cmd, args);
 	char *path = argv[1];
 	int n = atoi(argv[2]);
 
 	int m = n_lines(path);
-	printf("Hay que correr %d tareas\n", m);
-	// char* tasks[m] = input_read(path);
+	printf("Hay que ejecutar %d tareas\n", m);
+	char tasks[m][255];
+	input_read(tasks, path, m);
+	printf("tasks quedó como:\n");
+    for (int j = 0; j < m; j++)
+    {
+    	printf("%d: %s", j, tasks[j]);
+    }
+    printf("\n");
+
+    // char** aux = str_split(tasks[0], ' ');
+    
+    
+    
+
+
+    /* En tasks tenemos un string por cada tarea, pero como string, no como arreglo */
 }
--- doer/utils.c
+++ doer/utils.c
@@ -1,4 +1,7 @@
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
 
 
 
@@ -22,20 +25,69 @@ int n_lines(char* path) {
 }
 
 
-// char** input_read(char* path) {
-// 	FILE *fp1;
-//     char buff[255];
-//     char* pch;
-//     fp1 = fopen (path, "r");
+void input_read(char tasks[][255], char* path, int m) {
+	FILE *fp1;
+    char buff[255];
+    char* pch;
+    fp1 = fopen (path, "r");
+    int i = 0;
 
-//     while (fgets(buff, 255, (FILE*)fp1) != NULL){
-//     	char** params;
-//     	params = (char**) calloc(20, sizeof(char*))
-//     	pch = strtok(buff, " ");
-//     	int contador = 0;
-//         while (pch != NULL) {
-//         	params[contador] = pch;
-//         	pch = strtok(NULL, " ");
-//         }
-//     }
-// }
+    while (fgets(buff, 255, (FILE*)fp1) != NULL){
+    	strcpy(tasks[i++], buff);
+    	// printf("buff es %s\n", buff);
+    }
+
+}
+
+
+
+char** str_split(char* a_str, const char a_delim)
+{
+    char** result    = 0;
+    size_t count     = 0;
+    char* tmp        = a_str;
+    char* last_comma = 0;
+    char delim[2];
+    delim[0] = a_delim;
+    delim[1] = 0;
+
+    /* Count how many elements will be extracted. */
+    while (*tmp)
+    {
+        if (a_delim == *tmp)
+        {
+            count++;
+            last_comma = tmp;
+        }
+        tmp++;
+    }
+
+    /* Add space for trailing token. */
+    count += last_comma < (a_str + strlen(a_str) - 1);
+
+    /* Add space for terminating null string so caller
+       knows where the list of returned strings ends. */
+    count++;
+
+    result = malloc(sizeof(char*) * count);
+
+    if (result)
+    {
+        size_t idx  = 0;
+        char* token = strtok(a_str, delim);
+
+        while (token)
+        {
+            assert(idx < count);
+            *(result + idx++) = strdup(token);
+            token = strtok(0, delim);
+        }
+        assert(idx == count - 1);
+        *(result + idx) = 0;
+    }
+
+    return result;
+}
+
+
+
